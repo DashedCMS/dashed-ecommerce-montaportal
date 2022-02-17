d1e41d4c6e2c33da4a6c0f2e9d00802afcafbeb9
Robinvm <Robinvm@users.noreply.github.com>
2022-02-17 06:34:18 +0000

    Fix styling

--- src/QcommerceEcommerceMontaportalEventServiceProvider.php
+++ src/QcommerceEcommerceMontaportalEventServiceProvider.php
@@ -4,9 +4,7 @@ namespace Qubiqx\QcommerceEcommerceMontaportal;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Qubiqx\QcommerceEcommerceCore\Events\Orders\OrderMarkedAsPaidEvent;
-use Qubiqx\QcommerceEcommerceCore\Events\Products\ProductCreatedEvent;
 use Qubiqx\QcommerceEcommerceMontaportal\Listeners\MarkOrderAsPushableListener;
-use Qubiqx\QcommerceEcommerceMontaportal\Listeners\MarkProductAsPushableListener;
 
 class QcommerceEcommerceMontaportalEventServiceProvider extends ServiceProvider
 {
