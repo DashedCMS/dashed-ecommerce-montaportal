a6ef68f24e6f2e3f135f715f876f103b44b789db
Robin van Maasakker <robin@qubiqx.com>
2024-01-17 12:50:53 +0100

    Update

--- src/Livewire/Orders/ShowMontaportalOrder.php
+++ src/Livewire/Orders/ShowMontaportalOrder.php
@@ -2,6 +2,7 @@
 
 namespace Dashed\DashedEcommerceMontaportal\Livewire\Orders;
 
+use Filament\Notifications\Notification;
 use Livewire\Component;
 
 class ShowMontaportalOrder extends Component
@@ -20,30 +21,30 @@ class ShowMontaportalOrder extends Component
 
     public function submit()
     {
-        if (! $this->order->montaPortalOrder) {
-            $this->dispatch('notify', [
-                'status' => 'error',
-                'message' => 'De bestelling mag niet naar Montaportal gepushed worden.',
-            ]);
+        if (!$this->order->montaPortalOrder) {
+            Notification::make()
+                ->danger()
+                ->title('De bestelling mag niet naar Montaportal gepushed worden.')
+                ->send();
         } elseif ($this->order->montaPortalOrder->pushed_to_montaportal == 1) {
-            $this->dispatch('notify', [
-                'status' => 'error',
-                'message' => 'De bestelling is al naar Montaportal gepushed.',
-            ]);
+            Notification::make()
+                ->danger()
+                ->title('De bestelling is al naar Montaportal gepushed.')
+                ->send();
         } elseif ($this->order->montaPortalOrder->pushed_to_montaportal == 0) {
-            $this->dispatch('notify', [
-                'status' => 'error',
-                'message' => 'De bestelling wordt al naar Montaportal gepushed.',
-            ]);
+            Notification::make()
+                ->danger()
+                ->title('De bestelling wordt al naar Montaportal gepushed.')
+                ->send();
         }
 
         $this->order->montaPortalOrder->pushed_to_montaportal = 0;
         $this->order->montaPortalOrder->save();
 
         $this->dispatch('refreshPage');
-        $this->dispatch('notify', [
-            'status' => 'success',
-            'message' => 'De bestelling wordt binnen enkele minuten opnieuw naar Montaportal gepushed.',
-        ]);
+        Notification::make()
+            ->success()
+            ->title('De bestelling wordt binnen enkele minuten opnieuw naar Montaportal gepushed.')
+            ->send();
     }
 }
