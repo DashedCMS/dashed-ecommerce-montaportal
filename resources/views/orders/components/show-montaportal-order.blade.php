c9596f1a93cb630d81e36b597c69beff367eec5c
Robin van Maasakker <robin@qubiqx.com>
2024-07-08 10:20:41 +0200

    Update

--- resources/views/orders/components/show-montaportal-order.blade.php
+++ resources/views/orders/components/show-montaportal-order.blade.php
@@ -21,6 +21,12 @@
                 class="bg-yellow-100 text-yellow-800 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium">
                                 Bestelling wordt naar Montaportal gepushed
                                 </span>
+            <form wire:submit.prevent="deleteMontaportalOrder">
+                <button type="submit"
+                        class="inline-flex items-center justify-center font-medium tracking-tight rounded-lg focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 h-9 px-4 text-white shadow focus:ring-white w-full mt-2">
+                    Niet naar Montaportal pushen
+                </button>
+            </form>
         @endif
     @else
         <span
@@ -30,7 +36,7 @@
         <form wire:submit.prevent="createMontaportalOrder">
             <button type="submit"
                     class="inline-flex items-center justify-center font-medium tracking-tight rounded-lg focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 h-9 px-4 text-white shadow focus:ring-white w-full mt-2">
-                Opnieuw koppelen aan Montaportal pushen
+                Opnieuw koppelen aan Montaportal
             </button>
         </form>
     @endif
