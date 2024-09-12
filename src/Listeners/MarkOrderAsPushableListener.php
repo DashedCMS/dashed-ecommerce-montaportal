bd023d642fbf9ca8aab980ab8fa33be3beed09a3
Robin van Maasakker <robin@qubiqx.com>
2024-09-12 10:04:42 +0200

    Update

--- src/Listeners/MarkOrderAsPushableListener.php
+++ src/Listeners/MarkOrderAsPushableListener.php
@@ -29,21 +29,21 @@ class MarkOrderAsPushableListener
         if (Montaportal::isConnected($event->order->site_id) && $event->order->eligibleForFulfillmentProvider('montaportal')) {
             $event->order->montaPortalOrder()->create([]);
             $orderLog = new OrderLog();
-            $orderLog->order_id = $this->id;
+            $orderLog->order_id = $event->order->id;
             $orderLog->user_id = null;
             $orderLog->tag = 'system.note.created';
             $orderLog->note = 'Montaportal order created';
             $orderLog->save();
         } elseif (! Montaportal::isConnected($event->order->site_id)) {
             $orderLog = new OrderLog();
-            $orderLog->order_id = $this->id;
+            $orderLog->order_id = $event->order->id;
             $orderLog->user_id = null;
             $orderLog->tag = 'system.note.created';
             $orderLog->note = 'Montaportal order not created, site not connected';
             $orderLog->save();
         } elseif (! $event->order->eligibleForFulfillmentProvider('montaportal')) {
             $orderLog = new OrderLog();
-            $orderLog->order_id = $this->id;
+            $orderLog->order_id = $event->order->id;
             $orderLog->user_id = null;
             $orderLog->tag = 'system.note.created';
             $orderLog->note = 'Montaportal order not created, order not eligible for Montaportal';
